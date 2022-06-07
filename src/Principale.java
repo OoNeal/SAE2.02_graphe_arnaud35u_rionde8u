755b00e77e026b28b3a66723fdb1969ab4d58a43
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-07 11:45:12 +0200

    Modifications de la classe principale avec l'ajout d'un fichier texte pour le constructeur

--- src/Principale.java
+++ src/Principale.java
@@ -1,9 +1,17 @@
 package src;
 
+import java.io.IOException;
+
 public class Principale {
 
-    public static void main(String[] args) {
-        GrapheListe g = new GrapheListe("A");
+    public static void main(String[] args){
+        GrapheListe g = new GrapheListe("graphe.txt");
+
+        /*g.ajouterNoeud("A");
+        g.ajouterNoeud("B");
+        g.ajouterNoeud("C");
+        g.ajouterNoeud("D");
+        g.ajouterNoeud("E");
 
         g.ajouterArc("A","B", 12);
         g.ajouterArc("A","D",87);
@@ -11,8 +19,8 @@ public class Principale {
         g.ajouterArc("C","A",19);
         g.ajouterArc("D","B",23);
         g.ajouterArc("D","C",10);
-        g.ajouterArc("E","D",43);
+        g.ajouterArc("E","D",43);*/
 
-        System.out.println(g);
+        System.out.println(g.toGraphviz());
     }
 }
