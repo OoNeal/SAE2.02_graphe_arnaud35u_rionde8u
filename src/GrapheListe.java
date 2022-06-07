90b36f5e27aaec8e4920f49e2504acb5382af808
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-07 10:35:21 +0200

    Modifications de GrapheListe

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,74 +1,74 @@
 package src;
 
-import src.Arc;
-import src.Graphe;
-import src.Noeud;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GrapheListe implements Graphe {
-    private List<String> ensNom;
-    private List<Noeud> ensNoeuds;
+    private final List<String> ensNom;
+    private final List<Noeud> ensNoeuds;
 
-    public GrapheListe(List<String> ensNom, List<Noeud> ensNoeuds) {
-        this.ensNom = ensNom;
-        this.ensNoeuds = ensNoeuds;
+    public GrapheListe(String n) {
+        this.ensNom = new ArrayList<>();
+        this.ensNoeuds = new ArrayList<>();
+        this.ensNom.add(n);
+    }
+
+    public void ajouterArc(String depart, String destination, double cout) {
+        //si le nom du noeud est bien dans la liste
+        if (this.ensNom.contains(depart)) {
+            //si la destination n'est pas deja dans la liste, on l'ajoute
+            if (!this.ensNom.contains(destination)) {
+                this.ensNom.add(destination);
+            }
+
+            //on recupere le noeud de depart
+            Noeud noeud = new Noeud(depart);
+            //si la liste de noeud est vide, on l'ajoute et on ajoute l'arc
+            if (this.ensNoeuds.size() == 0) {
+                this.ensNoeuds.add(noeud);
+                noeud.ajouterArc(destination, cout);
+            } else {
+                // sinon on parcours la liste pour savoir si le noeud de depart existe deja
+                boolean trouve = false;
+                for (int i = 0; i < this.ensNoeuds.size() && !trouve; i++) {
+                    if (this.ensNoeuds.get(i).equals(noeud)) {
+                        this.ensNoeuds.get(i).ajouterArc(destination, cout);
+                        trouve = true;
+                    }
+                }
+                //si le noeud de depart n'existe pas, on l'ajoute et on ajoute l'arc
+                if (!trouve) {
+                    this.ensNoeuds.add(noeud);
+                    noeud.ajouterArc(destination, cout);
+                }
+            }
+        }
+
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Noeud noeud : this.ensNoeuds) {
+            s.append(noeud.getNom()).append(" -> ");
+            for (Arc arc : noeud.getAdj()) {
+                s.append(arc).append(" ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
     @Override
     public List<String> listeNoeuds() {
-        List<String> liste = new ArrayList<String>();
-        for (Noeud noeud : ensNoeuds)
-        {
-            liste.add(noeud.getNom());
-
-        }
-        return liste;
+        return ensNom;
     }
 
     @Override
     public List<Arc> suivants(String n) {
-        List<Arc> liste = new ArrayList<Arc>();
-        for (Noeud noeud : ensNoeuds)
-        {
+        for (Noeud noeud : ensNoeuds) {
             if (noeud.getNom().equals(n))
-            {
-                liste = noeud.getAdj();
-            }
+                return noeud.getAdj();
         }
-        return liste;
-    }
-
-    public void ajouterArc(String depart, String destination, double cout)
-    {
-        Noeud noeud = new Noeud(depart);
-        // verifier que les noeuds existent bien dans le graphe
-        if (ensNom.contains(depart) && ensNom.contains(destination))
-        {
-            noeud.ajouterArc(destination, cout);
-            ensNom.add(depart);
-            ensNoeuds.add(noeud);
-        }
-
-    }
-
-    public void ajouterNoeud(Noeud n)
-    {
-        if (!ensNom.contains(n.getNom()))
-        {
-            ensNom.add(n.getNom());
-            ensNoeuds.add(n);
-        }
-    }
-
-    public String toString()
-    {
-        StringBuilder sb = new StringBuilder();
-        for (Noeud noeud : ensNoeuds)
-        {
-            sb.append(noeud.getNom()).append(" -> ").append(noeud.getAdj()).append("\n");
-        }
-        return sb.toString();
+        return null;
     }
 }
