e50cc02093596fec7bbd91930a65e8f608447665
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-07 10:36:04 +0200

    Creation de la classe Principale (main)

---
+++ src/Principale.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Principale {
+
+    public static void main(String[] args) {
+        GrapheListe g = new GrapheListe("A");
+
+        g.ajouterArc("A","B", 12);
+        g.ajouterArc("A","D",87);
+        g.ajouterArc("B","E",11);
+        g.ajouterArc("C","A",19);
+        g.ajouterArc("D","B",23);
+        g.ajouterArc("D","C",10);
+        g.ajouterArc("E","D",43);
+
+        System.out.println(g);
+    }
+}
