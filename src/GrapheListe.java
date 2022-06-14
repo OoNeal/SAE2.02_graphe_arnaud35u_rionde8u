c43e8676137588fd9c309220bc3745ad6c59d9c6
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-14 13:08:35 +0200

    Modification des méthodes de GrapheListe

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -44,17 +44,6 @@ public class GrapheListe implements Graphe {
     }
 
     /**
-     * Permet d'ajouter le nom d'un noeud dans le graphe
-     *
-     * @param nom nom du noeud
-     */
-    public void ajouterNoeud(String nom) {
-        if (!this.ensNom.contains(nom)) {
-            this.ensNom.add(nom);
-        }
-    }
-
-    /**
      * Permet d'ajouter un arc dans le graphe
      * Si le noeud de depart existe et le noeud d'arrivee existe, alors on ajoute l'arc au noeud de departw
      * Sinon si le noeud d'arrivee n'existe pas on le cree et on ajoute l'arc
@@ -64,47 +53,15 @@ public class GrapheListe implements Graphe {
      * @param cout        cout de l'arc
      */
     public void ajouterArc(String depart, String destination, double cout) {
-        //si le nom du noeud de depart est bien dans la liste
-        if (this.ensNom.contains(depart)) {
-            //si la destination n'est pas deja dans la liste, on l'ajoute
-            if (!this.ensNom.contains(destination)) {
-                this.ensNom.add(destination);
-            }
-
-            //on recupere le noeud de depart
-            Noeud noeudDepart = new Noeud(depart);
-            //si la liste de noeud est vide, on ajoute le noeud de depart et on ajoute l'arc
-            if (this.ensNoeuds.size() == 0) {
-                this.ensNoeuds.add(noeudDepart);
-                noeudDepart.ajouterArc(destination, cout);
-            } else {
-                // sinon on parcours la liste pour savoir si le noeud de depart existe deja
-                boolean trouveNoeud = false;
-                boolean trouveArc = false;
-                for (Noeud noeud : this.ensNoeuds) {
-                    if (noeud.getNom().equals(depart)) {
-                        trouveNoeud = true;
-                        //on parcours la liste des arcs du noeud de depart
-                        for (Arc arc : noeud.getAdj()) {
-                            if (arc.getDest().equals(destination)) {
-                                trouveArc = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-                //si le noeud de depart n'existe pas, on l'ajoute et on ajoute l'arc
-                if (!trouveNoeud) {
-                    this.ensNoeuds.add(noeudDepart);
-                    noeudDepart.ajouterArc(destination, cout);
-                } else {
-                    //si l'arc n'existe pas deja, on l'ajoute
-                    if (!trouveArc) {
-                        noeudDepart.ajouterArc(destination, cout);
-                    }
-                }
-            }
+        if (!this.ensNom.contains(depart)) {
+            this.ensNom.add(depart);
+            this.ensNoeuds.add(new Noeud(depart));
         }
+        if (!this.ensNom.contains(destination)) {
+            this.ensNom.add(destination);
+            this.ensNoeuds.add(new Noeud(destination));
+        }
+        this.ensNoeuds.get(this.ensNom.indexOf(depart)).ajouterArc(destination, cout);
     }
 
     @Override
@@ -139,11 +96,11 @@ public class GrapheListe implements Graphe {
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Noeud noeud : this.ensNoeuds) {
-            s.append(noeud.getNom()).append(" -> ");
             for (Arc arc : noeud.getAdj()) {
+                s.append(noeud.getNom()).append(" -> ");
                 s.append(arc).append(" ");
+                s.append("\n");
             }
-            s.append("\n");
         }
         return s.toString();
     }
