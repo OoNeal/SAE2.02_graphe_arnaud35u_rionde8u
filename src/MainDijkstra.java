bc429ebd741f12239f1333f61828b77cfe0e839f
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-16 15:18:50 +0200

    Creation du MainDijkstra

---
+++ src/MainDijkstra.java
@@ -0,0 +1,17 @@
+package src;
+
+public class MainDijkstra {
+
+    public static void main(String[] args) {
+        GrapheListe g = new GrapheListe("document/graphe.txt");
+
+        System.out.println("/--------------------- ! Dijkstra ! --------------------\\");
+        Dijkstra d = new Dijkstra();
+        Valeur v = d.resoudre(g, "A");
+        System.out.println(v);
+
+        System.out.println("/--------------------- ! CalculerChemin ! --------------------\\");
+        System.out.println(v.calculerChemin("E"));
+        System.out.println(v.calculerChemin("D"));
+    }
+}
