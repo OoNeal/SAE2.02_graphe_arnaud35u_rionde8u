503ae7b4aed5d8164a2cb98a842bf134283b53d3
OoNeal <arnaud35u@etu.univ-lorraine.fr>
2022-06-16 15:17:02 +0200

    Ajout de l'algorithme de Dijkstra et de la méthode résoudre

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -5,32 +5,67 @@ import java.util.List;
 
 public class Dijkstra {
 
+    /*
+    Entrees :
+    G un graphe oriente avec une ponderation (poids) positive des arcs
+    A un sommet (depart) de G
+    Debut
+        Q <- {} // utilisation d’une liste de noeuds a traiter
+        Pour chaque sommet v de G faire
+            v.distance <- Infini
+            v.parent <- Indefini
+            Q <- Q U {v} // ajouter le sommet v a la liste Q
+        Fin Pour
+        A.distance <- 0
+        Tant que Q est un ensemble non vide faire
+            u <- un sommet de Q telle que u.distance est minimale
+            Q <- Q \ {u} // enlever le sommet u de la liste Q
+            Pour chaque sommet v de Q tel que l’arc (u,v) existe faire
+                D <- u.distance + poids(u,v)
+                Si D < v.distance
+                    Alors v.distance <- D
+                        v.parent <- u
+                Fin Si
+            Fin Pour
+        Fin Tant que
+     Fin
+     */
+
     public Valeur resoudre(Graphe g, String depart) {
-        List<Noeud> Q = new ArrayList<>();
-        for (String n : g.listeNoeuds()) {
-            Noeud noeud = new Noeud(n);
-            // noeud.setValeur(Double.POSITIVE_INFINITY);
-            //noeud.setParent(null);
-            Q.add(noeud);
+        Valeur v = new Valeur();
+        List<Noeud> ListeATraiter = new ArrayList<>();
+        for (String noeud : g.listeNoeuds()) {
+                v.setValeur(noeud, Double.MAX_VALUE);
+                ListeATraiter.add(new Noeud(noeud));
         }
-        Noeud noeudDepart = new Noeud(depart);
-        noeudDepart.ajouterArc(depart, 0);
-        while (Q != null) {
-            Noeud noeud = Q.get(0);
-            Q.remove(noeud);
-            for (Noeud n : Q) {
-                {
-                    //D = noeud.distance + poids(noeud, n)
-                    double D = noeud.getAdj().get(0).getCout() + g.listeNoeuds().size();
-                    if (D < n.getAdj().get(0).getCout()) {
-                        // n.setValeur(D);
-                        //n.setParent(noeud);
+        v.setValeur(depart, 0);
+
+        while (!ListeATraiter.isEmpty()) {
+            Noeud noeud = ListeATraiter.get(0);
+            double min = Double.MAX_VALUE;
+            for (Noeud n : ListeATraiter) {
+                min = Math.min(min, v.getValeur(n.getNom()));
+                if (min == v.getValeur(n.getNom())) {
+                    noeud = n;
+                }
+            }
+            ListeATraiter.remove(noeud);
+            for (Noeud noeudATraiter : ListeATraiter) {
+                double cout = -1;
+                for (Arc arc : g.suivants(noeud.getNom())) {
+                    if (arc.getDest().equals(noeudATraiter.getNom())) {
+                        cout = arc.getCout();
                     }
                 }
-
-
+                if (cout != -1) {
+                    double newDistance = v.getValeur(noeud.getNom()) + cout;
+                    if (newDistance < v.getValeur(noeudATraiter.getNom())) {
+                        v.setValeur(noeudATraiter.getNom(), newDistance);
+                        v.setParent(noeudATraiter.getNom(), noeud.getNom());
+                    }
+                }
             }
         }
-        return null;
+        return v;
     }
 }
